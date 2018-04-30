5e0b5756843e8023221dfe51f9a7123cf83fcae0
nicelite <37691684+nicelite@users.noreply.github.com>
2018-05-01 01:59:26 +0200

    Add files via upload

--- Model.py
+++ Model.py
@@ -4,19 +4,14 @@ Created on Fri Apr 20 17:44:46 2018
 @author: Batuhan
 """
 from keras.models import Model
-from keras.layers import Dense, Conv1D, MaxPooling1D, Concatenate, Input, Dropout, Masking, Reshape,Flatten
-import numpy as np
+from keras.constraints import max_norm
+from keras.layers import Dense, Conv1D, MaxPooling1D, Concatenate, Input, Dropout,Flatten
 
 
-def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, num_filters, l2_reg):
+
+def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, num_filters, dropout_rate,l2_reg):
 
     inputs = Input(shape = (max_sequence_length,embedding_size,))
-#    input_sentence = Reshape(target_shape = (max_sequence_length, embedding_size))(inputs)
-#    mask = Masking(mask_value = float('nan'),
-#                   input_shape = (max_sequence_length, embedding_size))(input_sentence)
-#    sequence_length = K.shape(mask).eval(session = session)[0]
-        
-#    input_sentence = Reshape(target_shape = (max_sequence_length, embedding_size))(inputs)
 
     # Construction of Convolutional layer
     convs = []
@@ -24,19 +19,20 @@ def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, n
         conv = Conv1D(filters = num_filters,
                       kernel_size = filter_size,
                       activation = "relu",
-                      padding = "valid")(inputs)
+                      padding = "valid",
+                      kernel_constraint=max_norm(l2_reg))(inputs)
         pooled = MaxPooling1D(pool_size =max_sequence_length-filter_size+1,
                               strides = 1,
                               padding = "valid")(conv)
         convs.append(pooled)
 
-#    concatenate_layer = Concatenate(axis = 0)(convs)
+    concatenate_layer = Concatenate(axis = 1)(convs)
 
-    flat_layer = Flatten()(pooled)
+    flat_layer = Flatten()(concatenate_layer)
     
     # Dropout
 
-    drop_layer = Dropout(0.5)(flat_layer)
+    drop_layer = Dropout(dropout_rate)(flat_layer)
     
 
     # Fully-connected layer
@@ -45,10 +41,3 @@ def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, n
 
     model = Model(inputs = inputs, outputs = predictions)
     return model
-
-#tf_session = K.get_session()
-#model1 = build_model(200,2, 300,[2,3,4], 100, 0)
-#model1.compile(optimizer='adam',
-#              loss='categorical_crossentropy',
-#              metrics=['accuracy'])
-#model1.summary()

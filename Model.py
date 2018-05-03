277cf35a0f0a5775562754a2a556f2a6a708142a
nicelite <37691684+nicelite@users.noreply.github.com>
2018-05-03 14:51:25 +0200

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 20 17:44:46 2018
-@author: Batuhan
-"""
+
+import numpy as np
 from keras.models import Model
 from keras.constraints import max_norm
 from keras.layers import Dense, Conv1D, MaxPooling1D, Concatenate, Input, Dropout,Flatten
-
 
 
 def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, num_filters, dropout_rate,l2_reg):
@@ -19,8 +15,7 @@ def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, n
         conv = Conv1D(filters = num_filters,
                       kernel_size = filter_size,
                       activation = "relu",
-                      padding = "valid",
-                      kernel_constraint=max_norm(l2_reg))(inputs)
+                      padding = "valid")(inputs)
         pooled = MaxPooling1D(pool_size =max_sequence_length-filter_size+1,
                               strides = 1,
                               padding = "valid")(conv)
@@ -34,10 +29,12 @@ def build_model(max_sequence_length,num_classes, embedding_size, filter_sizes, n
 
     drop_layer = Dropout(dropout_rate)(flat_layer)
     
-
     # Fully-connected layer
 
-    predictions = Dense(num_classes, activation = "softmax")(drop_layer)
+    predictions = Dense(num_classes, 
+						activation = "softmax",
+						kernel_constraint=max_norm(l2_reg))(drop_layer)
 
     model = Model(inputs = inputs, outputs = predictions)
     return model
+
